800d18cd256adcd344e0de13532911afe2cfc2c7
MATHEUS SOUSA ALVES <matheus.salves14@senacsp.edu.br>
2021-09-30 09:29:30 -0300

    Adicionar arquivos de projeto.

---
+++ CalculoConsumo/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿
+namespace CalculoConsumo
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtkm = new System.Windows.Forms.TextBox();
+            this.txtlitros = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblrendimento = new System.Windows.Forms.Label();
+            this.lblconsumo = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btmcalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Typo Square Regular Demo", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(300, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(269, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Calculo de Consumo de Veículos";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(188, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "KM:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(188, 190);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Litros:";
+            // 
+            // txtkm
+            // 
+            this.txtkm.Location = new System.Drawing.Point(233, 128);
+            this.txtkm.Name = "txtkm";
+            this.txtkm.Size = new System.Drawing.Size(119, 23);
+            this.txtkm.TabIndex = 3;
+            // 
+            // txtlitros
+            // 
+            this.txtlitros.Location = new System.Drawing.Point(233, 187);
+            this.txtlitros.Name = "txtlitros";
+            this.txtlitros.Size = new System.Drawing.Size(119, 23);
+            this.txtlitros.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(532, 125);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(113, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Rendimento - KM";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(532, 203);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(134, 20);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Consumo - Litros/KM";
+            // 
+            // lblrendimento
+            // 
+            this.lblrendimento.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.lblrendimento.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblrendimento.Location = new System.Drawing.Point(512, 149);
+            this.lblrendimento.Name = "lblrendimento";
+            this.lblrendimento.Size = new System.Drawing.Size(158, 25);
+            this.lblrendimento.TabIndex = 7;
+            // 
+            // lblconsumo
+            // 
+            this.lblconsumo.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.lblconsumo.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblconsumo.Location = new System.Drawing.Point(512, 231);
+            this.lblconsumo.Name = "lblconsumo";
+            this.lblconsumo.Size = new System.Drawing.Size(158, 23);
+            this.lblconsumo.TabIndex = 8;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.groupBox1.Location = new System.Drawing.Point(127, 100);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(272, 181);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Insira as informações";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.groupBox2.Location = new System.Drawing.Point(446, 100);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(272, 181);
+            this.groupBox2.TabIndex = 10;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Resultados";
+            // 
+            // btmcalcular
+            // 
+            this.btmcalcular.Location = new System.Drawing.Point(395, 325);
+            this.btmcalcular.Name = "btmcalcular";
+            this.btmcalcular.Size = new System.Drawing.Size(88, 38);
+            this.btmcalcular.TabIndex = 11;
+            this.btmcalcular.Text = "Calcular";
+            this.btmcalcular.UseVisualStyleBackColor = true;
+            this.btmcalcular.Click += new System.EventHandler(this.btmcalcular_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btmcalcular;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(846, 411);
+            this.Controls.Add(this.btmcalcular);
+            this.Controls.Add(this.lblconsumo);
+            this.Controls.Add(this.lblrendimento);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtlitros);
+            this.Controls.Add(this.txtkm);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculo Consumo";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtkm;
+        private System.Windows.Forms.TextBox txtlitros;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblrendimento;
+        private System.Windows.Forms.Label lblconsumo;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btmcalcular;
+    }
+}
+
---
+++ CalculoConsumo/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculoConsumo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        public decimal CalculoConsumo(TextBox num)
+        {
+            try
+            {
+                return Convert.ToDecimal(num.Text);
+            }
+            catch
+            {
+                num.Focus();
+                num.SelectAll();
+                throw new Exception("Preencha corretamente o campo" + num.Name.ToUpper().Replace("TEXTBOX", ""));           
+            }
+        }
+        public void Limpartela()
+        {
+            foreach( Control ctl in this.Controls)
+            {
+                if(ctl is TextBox)
+                {
+                    ctl.Text = String.Empty;
+                }else if(ctl is Label && Convert.ToInt32(ctl.Tag).Equals(1))
+                {
+                    ctl.Text = String.Empty;
+                }
+            }
+        }
+
+        private void btmcalcular_Click(object sender, EventArgs e)
+        {
+            try 
+            {
+                decimal KM = CalculoConsumo(txtkm);
+                decimal Litros = CalculoConsumo(txtlitros);
+
+                decimal Rendimento = KM / Litros;
+                decimal Consumo = Litros / KM;
+
+                lblrendimento.Text = Rendimento.ToString("N1");
+                lblconsumo.Text = Consumo.ToString("N3");
+
+
+
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Atenção");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
